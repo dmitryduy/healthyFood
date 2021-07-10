53973c3e9279cd72de770a65b2079a0a81c7a9de
dmitryduy <cdcvh14@gmail.com>
2021-07-10 17:12:00 +0300

    add header

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const {src, dest, watch, parallel} = require('gulp')
+const scss = require('gulp-sass')(require('sass'))
+const concat = require('gulp-concat')
+const browserSync = require('browser-sync').create();
+
+function browser() {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+}
+
+function styles() {
+    return src('app/scss/*.scss')
+        .pipe(scss({outputStyle: 'compressed'}))
+        .pipe(concat('style.min.css'))
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream())
+}
+
+function watching() {
+    watch(['app/scss/*.scss'], styles)
+    watch(['app/*.html']).on('change', browserSync.reload)
+}
+
+exports.styles = styles;
+exports.watching = watching;
+exports.browser = browser;
+
+exports.default = parallel(browser, watching)
