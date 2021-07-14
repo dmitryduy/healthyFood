8f1892d52eb7b1a2e104243fdbffcb79de8635d4
dmitryduy <cdcvh14@gmail.com>
2021-07-14 15:33:17 +0300

    add adaptive + popup menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,10 @@
 const prevSliderButton = document.querySelector('.reviews__prev-button');
 const nextSliderButton = document.querySelector('.reviews__next-button');
 const sliderContainer = document.querySelector('.reviews__slider-list');
+const popUpContent = document.querySelector('.pop-up__content');
+const popUp = document.querySelector('.pop-up');
+const popUpButton = document.querySelector('.pop-up__close');
+const hamburger = document.querySelector('.header__hamburger');
 let currentSlide = 0;
 
 nextSliderButton.addEventListener('click', () => {
@@ -30,4 +34,22 @@ prevSliderButton.addEventListener('click', () => {
 
 window.addEventListener('resize', () => {
     sliderContainer.style.transform = `translateX(-${currentSlide* sliderContainer.clientWidth}px)`;
+})
+
+
+popUp.addEventListener('click', (e) => {
+    console.log(e.path)
+    if (e.path.includes(popUpContent) && !e.path.includes(popUpButton)) {
+        return;
+    }
+    document.body.style.overflow = 'auto';
+    popUp.style.visibility = 'hidden';
+    popUpContent.style.transform = 'translateX(-200px)';
+})
+
+
+hamburger.addEventListener('click', () => {
+    popUp.style.visibility = 'visible';
+    popUpContent.style.transform = 'translateX(0)';
+    document.body.style.overflow = 'hidden';
 })
